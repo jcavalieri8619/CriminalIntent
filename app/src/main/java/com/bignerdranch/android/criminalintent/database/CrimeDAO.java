36f38fa58ea5f881dd56b38fcecc744c21f23687
John P Cavalieri <jcavalieri8619@gmail.com>
2018-06-02 08:32:04 -0400

    removed iteration of UUID sinces can't be converted to Integer/Long

--- app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDAO.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDAO.java
@@ -4,11 +4,10 @@ import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
-import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
-import com.bignerdranch.android.criminalintent.models.Crime;
+import com.bignerdranch.android.criminalintent.database.entity.CrimeEntity;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,38 +18,38 @@ import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
 public interface CrimeDAO {
 
     @Query("SELECT * FROM crimesDB")
-    LiveData<List<Crime>> getAll();
+    LiveData<List<CrimeEntity>> getAll();
 
-    @Query("SELECT * FROM crimesDB WHERE uuid_column IN (:crimeIDs)")
-    LiveData<List<Crime>> loadAllByIds(UUID[] crimeIDs);
+//    @Query("SELECT * FROM crimesDB WHERE uuid_column IN (:crimeIDs)")
+//    List<CrimeEntity> loadAllByIds(UUID[] crimeIDs);
 
 
     @Query("SELECT * FROM crimesDB WHERE is_serious_column == :isSerious")
-    LiveData<List<Crime>> loadAllBySerious(boolean isSerious);
+    LiveData<List<CrimeEntity>> loadAllBySerious(boolean isSerious);
 
 
     @Query("SELECT * FROM crimesDB WHERE uuid_column LIKE :crime_uuid "
             + "LIMIT 1")
-    LiveData<Crime> findByUUID(UUID crime_uuid);
+    LiveData<CrimeEntity> findByUUID(UUID crime_uuid);
 
 
     @Query("SELECT * FROM crimesDB WHERE title_column LIKE :title LIMIT 1")
-    LiveData<Crime> findByTitle(String title);
+    LiveData<CrimeEntity> findByTitle(String title);
 
 
     @Query("SELECT * FROM crimesDB WHERE is_solved_column == :isSolved")
-    LiveData<List<Crime>> loadAllBySolved(boolean isSolved);
+    LiveData<List<CrimeEntity>> loadAllBySolved(boolean isSolved);
 
 
 
 
     @Insert(onConflict = REPLACE)
-    void insert(Crime... crimes);
+    void insert(CrimeEntity... crimes);
 
     @Delete
-    void delete(Crime crime);
+    void delete(CrimeEntity... crimes);
 
     @Update(onConflict = REPLACE)
-    void update(Crime... users);
+    void update(CrimeEntity... crimes);
 
 }
