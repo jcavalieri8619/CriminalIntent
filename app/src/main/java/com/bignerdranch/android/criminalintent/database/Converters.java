76e0d749f68f72d09fd44b65df4dc3ec5eb91218
John P Cavalieri <jcavalieri8619@gmail.com>
2018-06-02 08:31:25 -0400

    changed conversion method from UUID to String instead of Long

--- app/src/main/java/com/bignerdranch/android/criminalintent/database/Converters.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/Converters.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.criminalintent.database;
 
 import android.arch.persistence.room.TypeConverter;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import java.util.Date;
 import java.util.Hashtable;
@@ -14,63 +15,72 @@ import java.util.stream.Collectors;
 public class Converters {
 
 
-    private static Map<Long, String> sLongStringMap = new Hashtable<>();
-    private static Map<String, Long> sStringLongMap = new Hashtable<>();
+//    private static Map<Long, String> sLongStringMap = new Hashtable<>();
+//    private static Map<String, Long> sStringLongMap = new Hashtable<>();
 
 
+    private static final String TAG = Converters.class.getSimpleName();
 
 
 
 
     @TypeConverter
-    public static UUID fromHashUUID( Long value) {
-        return value==null?null: UUID.fromString(sLongStringMap.get(value));
+    public static UUID fromHashUUID( String value) {
+
+        Log.d(TAG, "fromHashUUID: ");
+
+        return value==null?null: UUID.fromString(value);
     }
 
     @TypeConverter
-    public static Long UUIDToHash(UUID uuid) {
+    public static String UUIDToHash(UUID uuid) {
+        Log.d(TAG, "UUIDToHash: ");
+
         if (uuid == null) {
 
             return null;
         }
-        String key = addUUID(uuid);
 
-        return sStringLongMap.get(key);
+
+        return uuid.toString();
 
     }
 
 
     @TypeConverter
     public static Date fromTimestamp(Long value) {
+        Log.d(TAG, "fromTimestamp: ");
         return value == null ? null : new Date(value);
     }
 
     @TypeConverter
     public static Long dateToTimestamp(Date date) {
+        Log.d(TAG, "dateToTimestamp: ");
+
         return date == null ? null : date.getTime();
     }
 
-    private static String addUUID(UUID uuid) {
-        String uuid_str = uuid.toString();
-
-        if (sStringLongMap.containsKey(uuid_str)) {
-            return uuid_str;
-        }
-
-
-        Random rng = new Random(uuid.getMostSignificantBits());
-
-        Long uuid_long = rng.nextLong();
-        if (sLongStringMap.containsKey(uuid_long)) {
-            uuid_long = rng.nextLong();
-        }
-
-        sLongStringMap.put(uuid_long, uuid_str);
-        sStringLongMap.put(uuid_str, uuid_long);
-
-        return uuid_str;
-
-    }
+//    private static String addUUID(UUID uuid) {
+//        String uuid_str = uuid.toString();
+//
+//        if (sStringLongMap.containsKey(uuid_str)) {
+//            return uuid_str;
+//        }
+//
+//
+//        Random rng = new Random(uuid.getMostSignificantBits());
+//
+//        Long uuid_long = rng.nextLong();
+//        if (sLongStringMap.containsKey(uuid_long)) {
+//            uuid_long = rng.nextLong();
+//        }
+//
+//        sLongStringMap.put(uuid_long, uuid_str);
+//        sStringLongMap.put(uuid_str, uuid_long);
+//
+//        return uuid_str;
+//
+//    }
 
 
     //TODO didn't need this but its cool so I'll leave it here for now
