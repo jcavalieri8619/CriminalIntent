0bb461b693daa96d64d45ddfddac559d58092ac1
John P Cavalieri <jcavalieri8619@gmail.com>
2018-06-02 08:53:36 -0400

    supplies view model's with Repo

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/UI/ViewModelFactory.java
@@ -0,0 +1,38 @@
+package com.bignerdranch.android.criminalintent.UI;
+
+import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+
+import com.bignerdranch.android.criminalintent.UI.cimedetail.DetailViewModel;
+import com.bignerdranch.android.criminalintent.UI.crimelist.ListViewModel;
+import com.bignerdranch.android.criminalintent.repository.CrimeRepository;
+
+public class ViewModelFactory implements android.arch.lifecycle.ViewModelProvider.Factory {
+    CrimeRepository mDataSource;
+
+
+    public ViewModelFactory(final CrimeRepository dataSource) {
+        mDataSource = dataSource;
+    }
+
+    /**
+     * Creates a new instance of the given {@code Class}.
+     * <p>
+     *
+     * @param modelClass a {code Class} whose instance is requested
+     * @return a newly created ViewModel
+     */
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull final Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(ListViewModel.class)) {
+            return (T) new ListViewModel(mDataSource);
+        }else if (modelClass.isAssignableFrom(DetailViewModel.class)){
+            return (T) new DetailViewModel(mDataSource);
+
+        }
+
+        throw new IllegalArgumentException("Unknown ViewModel class");
+    }
+
+}
