c4928733e8d4db5b51cb60f922b5835b5a646a99
John P Cavalieri <jcavalieri8619@gmail.com>
2018-06-05 22:30:13 -0400

    modified listViewModel factory so it can inherit from AndroidViewModel

--- app/src/main/java/com/bignerdranch/android/criminalintent/UI/ViewModelFactory.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/UI/ViewModelFactory.java
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.criminalintent.UI;
 
+import android.app.Activity;
+import android.app.Application;
 import android.arch.lifecycle.ViewModel;
+import android.content.Context;
 import android.support.annotation.NonNull;
 
 import com.bignerdranch.android.criminalintent.UI.crimedetail.DetailPagerViewModel;
@@ -10,9 +13,13 @@ import com.bignerdranch.android.criminalintent.repository.CrimeRepository;
 public class ViewModelFactory implements android.arch.lifecycle.ViewModelProvider.Factory {
     CrimeRepository mDataSource;
 
+    Activity mActivity;
 
-    public ViewModelFactory(final CrimeRepository dataSource) {
+
+    public ViewModelFactory(final CrimeRepository dataSource, Activity activity) {
         mDataSource = dataSource;
+        this.mActivity = activity;
+
     }
 
     /**
@@ -26,7 +33,7 @@ public class ViewModelFactory implements android.arch.lifecycle.ViewModelProvide
     @Override
     public <T extends ViewModel> T create(@NonNull final Class<T> modelClass) {
         if (modelClass.isAssignableFrom(ListViewModel.class)) {
-            return (T) new ListViewModel(mDataSource);
+            return (T) new ListViewModel(mDataSource, mActivity.getApplication());
         }else if (modelClass.isAssignableFrom(DetailPagerViewModel.class)){
             return (T) new DetailPagerViewModel(mDataSource);
 
