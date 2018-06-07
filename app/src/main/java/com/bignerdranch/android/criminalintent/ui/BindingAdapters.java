f0f27d80110bee8dfe47312248f9f2c6ae9ff68a
John P Cavalieri <jcavalieri8619@gmail.com>
2018-06-07 15:13:33 -0400

    refactored

--- app/src/main/java/com/bignerdranch/android/criminalintent/ui/BindingAdapters.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/ui/BindingAdapters.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.bignerdranch.android.criminalintent.UI;
+package com.bignerdranch.android.criminalintent.ui;
 
 import android.databinding.BindingAdapter;
 import android.view.View;
